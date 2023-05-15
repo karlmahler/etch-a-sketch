e4f47d78ea400447d10f13b2dc4743db3b8551f6
Hasley <raudineac101@gmail.com>
2023-05-14 21:10:52 -0400

    Allow users to specify the grid size
    Let the user set the number of squares in the grid, instead of using a
    fixed size of 16x16.

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 ( function () {
-	function generateGrid() {
-		const grid = document.querySelector( '.grid-wrapper' );
-		const gridSize = 16;
+	const grid = document.querySelector( '.grid-wrapper' );
 
+	function generateGrid( gridSize = 16 ) {
 		for ( let row = 0; row < gridSize; row++ ) {
 			for ( let col = 0; col < gridSize; col++ ) {
 				const square = document.createElement( 'div' );
@@ -17,13 +16,56 @@
 		event.target.classList.add( 'square-color' );
 	}
 
-	function main() {
-		generateGrid();
-
+	function addMouseOverListener() {
 		const squares = document.querySelectorAll( '.square' );
 		squares.forEach( square => {
 			square.addEventListener( 'mouseover', handleMouseOver );
 		} );
+	}
+
+	function getGridSize() {
+		const promptMessage = 'Enter a number between 1 and 100';
+		const minSize = 1, maxSize = 100;
+		let gridSize = prompt( promptMessage );
+
+		while ( gridSize !== null ) {
+			gridSize = parseInt( gridSize );
+
+			if (
+				!isNaN( gridSize ) &&
+				gridSize >= minSize &&
+				gridSize <= maxSize
+			) {
+				return gridSize;
+			}
+
+			gridSize = prompt( `Invalid value! ${promptMessage}` );
+		}
+
+		return null;
+	}
+
+	function resizeGrid() {
+		const size = getGridSize();
+
+		if ( !size ) {
+			return;
+		}
+
+		// Clear container's content.
+		grid.textContent = '';
+		grid.style.setProperty( 'grid-template-columns', `repeat(${size}, 1fr)` );
+
+		generateGrid( size );
+		addMouseOverListener();
+	}
+
+	function main() {
+		generateGrid();
+		addMouseOverListener();
+
+		const resizeButton = document.getElementById( 'resize-grid' );
+		resizeButton.addEventListener( 'click', resizeGrid );
 	}
 
 	main();
