683341b3cf723117ac66ffe5d4982ca13fb571a4
Hasley <raudineac101@gmail.com>
2023-05-22 13:12:50 -0400

    Add new drawing options
    Enhance drawing functionality with three new modes—shade, random color,
    and erase—, and also add a button to clear and reset the grid area.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 ( function () {
+	const COLORS = { black: 'rgb(0, 0, 0)' };
 	const grid = document.querySelector( '.grid-wrapper' );
+	const config = { gridSize: 16 };
 
-	function generateGrid( gridSize = 16 ) {
-		for ( let row = 0; row < gridSize; row++ ) {
-			for ( let col = 0; col < gridSize; col++ ) {
+	function generateGrid() {
+		for ( let row = 0; row < config.gridSize; row++ ) {
+			for ( let col = 0; col < config.gridSize; col++ ) {
 				const square = document.createElement( 'div' );
 				square.classList.add( 'square' );
 
@@ -12,8 +14,95 @@
 		}
 	}
 
+	function isBackgroundBlack( event ) {
+		return event.target.style.getPropertyValue( 'background-color' ) === COLORS.black;
+	}
+
+	function addBlackBackground( event ) {
+		if ( isBackgroundBlack( event ) ) {
+			return;
+		}
+
+		event.target.style.setProperty( 'background-color', COLORS.black );
+	}
+
+	function isShade( event ) {
+		const OPACITY_REGEX = /^rgba\(0, 0, 0, \d(\.\d)?\)$/;
+		return OPACITY_REGEX.test( event.target.style.getPropertyValue( 'background-color' ) );
+	}
+
+	function getOpacity( event ) {
+		const OPACITY_INDEX_START = -4;
+		const OPACITY_INDEX_END = -1;
+
+		const opacity = Number(
+			event.target.style
+				.getPropertyValue( 'background-color' )
+				.slice( OPACITY_INDEX_START, OPACITY_INDEX_END )
+		);
+
+		return opacity;
+	}
+
+	function addShadeBackground( event ) {
+		if ( isBackgroundBlack( event ) ) {
+			return;
+		}
+
+		if ( !isShade( event ) ) {
+			const INITIAL_OPACITY = 'rgba(0, 0, 0, 0.1)';
+			event.target.style.setProperty( 'background-color', INITIAL_OPACITY );
+
+			return;
+		}
+
+		const OPACITY_STEP = 0.1;
+		const currentOpacity = getOpacity( event );
+		event.target.style.setProperty( 'background-color', `rgba(0, 0, 0, ${currentOpacity + OPACITY_STEP})` );
+	}
+
+	function getRandomRgbValue() {
+		const MAX_VALUE = 255;
+		const randomValue = Math.floor( Math.random() * MAX_VALUE + 1 );
+
+		return randomValue;
+	}
+
+	function getRandomRgb() {
+		const [ red, green, blue ] = [
+			getRandomRgbValue(),
+			getRandomRgbValue(),
+			getRandomRgbValue()
+		];
+		const rgb = `rgb(${red}, ${green}, ${blue})`;
+
+		return rgb;
+	}
+
+	function addRandomRgbBackground( event ) {
+		event.target.style.setProperty( 'background-color', getRandomRgb() );
+	}
+
+	function eraseBackground( event ) {
+		event.target.style.setProperty( 'background-color', '' );
+	}
+
+	config.currentDrawingMode = addBlackBackground;
+
+	function handleMode( event ) {
+		if ( event.target.value === 'shade' ) {
+			config.currentDrawingMode = addShadeBackground;
+		} else if ( event.target.value === 'random-color' ) {
+			config.currentDrawingMode = addRandomRgbBackground;
+		} else if ( event.target.value === 'erase' ) {
+			config.currentDrawingMode = eraseBackground;
+		} else {
+			config.currentDrawingMode = addBlackBackground;
+		}
+	}
+
 	function handleMouseOver( event ) {
-		event.target.classList.add( 'square-color' );
+		config.currentDrawingMode( event );
 	}
 
 	function addMouseOverListener() {
@@ -23,9 +112,18 @@
 		} );
 	}
 
+	function createLayout() {
+		generateGrid();
+		addMouseOverListener();
+	}
+
+	function clearGrid() {
+		grid.textContent = '';
+	}
+
 	function getGridSize() {
-		const promptMessage = 'Enter a number between 1 and 100';
-		const minSize = 1, maxSize = 100;
+		const minSize = 2, maxSize = 100;
+		const promptMessage = `Enter a number between ${minSize} and ${maxSize}`;
 		let gridSize = prompt( promptMessage );
 
 		while ( gridSize !== null ) {
@@ -45,6 +143,10 @@
 		return null;
 	}
 
+	function setGridSize( size ) {
+		config.gridSize = size;
+	}
+
 	function resizeGrid() {
 		const size = getGridSize();
 
@@ -52,20 +154,32 @@
 			return;
 		}
 
-		// Clear container's content.
-		grid.textContent = '';
-		grid.style.setProperty( 'grid-template-columns', `repeat(${size}, 1fr)` );
+		setGridSize( size );
+		clearGrid();
+		grid.style.setProperty( 'grid-template-columns', `repeat(${config.gridSize}, 1fr)` );
+		createLayout();
+	}
 
-		generateGrid( size );
-		addMouseOverListener();
+	function resetGrid() {
+		clearGrid();
+		createLayout();
 	}
 
 	function main() {
-		generateGrid();
-		addMouseOverListener();
+		createLayout();
 
 		const resizeButton = document.getElementById( 'resize-grid' );
 		resizeButton.addEventListener( 'click', resizeGrid );
+
+		const resetButton = document.getElementById( 'reset-grid' );
+		resetButton.addEventListener( 'click', resetGrid );
+
+		const radioInputs = document.querySelectorAll( 'input[type="radio"][name="drawing-mode"]' );
+		radioInputs.forEach( radioInput => {
+			radioInput.addEventListener( 'change', event => {
+				handleMode( event );
+			} );
+		} );
 	}
 
 	main();
